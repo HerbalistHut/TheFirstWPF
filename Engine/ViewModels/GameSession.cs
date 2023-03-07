d2c03fa03a9ada8b9edc70c55eb1e0e8c96d4969
Nikita Volodchenko <1volodchenko@mail.ru>
2023-03-07 11:01:33 +0300

    Added quests

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -33,6 +33,7 @@ namespace Engine.ViewModels
             CurrentWorld = WorldFactory.CreateWorld();
 
             CurrentLocation = CurrentWorld.LocationAt(0, -1);
+
         }
 
         public bool HasLocationToNorth
