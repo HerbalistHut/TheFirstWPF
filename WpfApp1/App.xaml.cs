7dc3375e07cd6b5d4b1725c898bed2ae44ba0abf
Nikita Volodchenko <1volodchenko@mail.ru>
2023-07-20 22:49:44 +0300

    +error log

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Engine.Services;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -13,5 +15,13 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText = $"An exception occured: {e.Exception.Message}\n at: {e.Exception.StackTrace}";
+
+            LoggingService.Log(e.Exception);
+
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 }
